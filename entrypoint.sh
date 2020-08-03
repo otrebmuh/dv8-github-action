98292320364cad9672c32e54f35e0a86e0d0c490
Humberto Cervantes <humberto.cervantes@gmail.com>
2020-08-03 13:06:34 -0500

    updated to use depends

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/sh -l
 
 echo "Running depends"
-/opt/java/openjdk/bin/java -jar /depends.jar
+/opt/java/openjdk/bin/java -jar /depends.jar -s -p dot -d . $1 src depends
+
+FILE=/depends.json
+if [ -f "$FILE" ]; then
+    echo "depends analysis success"
+else 
+    echo "depends analysis failed"
+fi
