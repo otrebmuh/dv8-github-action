78b32d6813acde51682cc83626ffdaf956373e75
Humberto Cervantes <humberto.cervantes@gmail.com>
2020-08-04 14:24:31 -0500

    Integrated DV8

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,17 @@
 #!/bin/sh -l
 
+echo "Activating DV8 with code: "$3
+/usr/local/bin/dv8-console license:activate -licenseKey $2 -activationCode $3
+
 echo "Running depends"
 /opt/java/openjdk/bin/java -jar /depends.jar -s -p dot -d . $1 src depends
+
+echo "Generating arch report"
+/usr/local/bin/dv8-console arch-report -paramsFile archreport.properties
+
+echo "Dectivating DV8"
+/usr/local/bin/dv8-console license:deactivate -activationCode $3
+
 
 echo "Current directory :"$(pwd)
 find . | grep depends.json
